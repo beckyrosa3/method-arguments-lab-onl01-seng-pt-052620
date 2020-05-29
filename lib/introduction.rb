e94dccaf4fb7d956bd004a6b2a9ff8496392a4d7
Becky Rosa <becky_m_rosa@yahoo.com>
2020-05-29 04:07:14 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,7 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
 def introduction_with_language (name, language)
  x.time do
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
